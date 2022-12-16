bd12fbd0ecb2a6ce622cc326df0386c9b2c0f28c
Vinicius Coscia <vinicius.coscia@live.com>
2022-12-16 18:17:00 -0300

    Finished Task 4

--- udacity/introduction/challenge/Task4.py
+++ udacity/introduction/challenge/Task4.py
@@ -12,6 +12,35 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+TELEMARKETING_PREFIX = '140'
+telemarketers = set()
+false_flags = set()
+
+for text in texts:
+    origin = str(text[0])
+    destination = str(text[1])
+
+    if origin.startswith(TELEMARKETING_PREFIX):
+        false_flags.add(origin)
+    elif destination.startswith(TELEMARKETING_PREFIX):
+        false_flags.add(destination)
+
+for call in calls:
+    origin = str(call[0])
+    destination = str(call[1])
+
+    if origin.startswith(TELEMARKETING_PREFIX):
+        telemarketers.add(origin)
+    if destination.startswith(TELEMARKETING_PREFIX):
+        false_flags.add(destination)
+
+for false_flag in false_flags:
+    telemarketers.discard(false_flag)
+
+print("These numbers could be telemarketers: ")
+for telemarketer in sorted(telemarketers):
+    print(telemarketer)
+
 """
 TASK 4:
 The telephone company want to identify numbers that might be doing
