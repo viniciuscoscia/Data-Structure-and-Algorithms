54f28e04f3c351329a3c138e526def04a48eba29
Vinicius Coscia <vinicius.coscia@live.com>
2022-12-16 14:16:01 -0300

    Task 1 completed

--- udacity/introduction/challenge/Task1.py
+++ udacity/introduction/challenge/Task1.py
@@ -1,16 +1,25 @@
+import csv
 """
 Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
-import csv
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
+    unique_numbers = set()
+    for text in texts:
+        unique_numbers.add(text[0])
+        unique_numbers.add(text[1])
 
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
+    for call in calls:
+        unique_numbers.add(call[0])
+        unique_numbers.add(call[1])
 
+print("There are " + str(len(unique_numbers)) + " different telephone numbers in the records.")
 
 """
 TASK 1:
