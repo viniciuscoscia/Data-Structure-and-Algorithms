ab51386d81a1d6271194243da8fb61346cae13e2
Vinicius Coscia <vinicius.coscia@live.com>
2025-01-08 16:18:18 -0300

    Adding ContainsDuplicate

---
+++ leetcode/217ContainsDuplicate.py
@@ -0,0 +1,12 @@
+from operator import truediv
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dic = {}
+        for num in nums:
+            if num not in dic:
+                dic[num] = 0
+            else:
+                return True
+        return False
--- leetcode/removeDuplicates.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-
-        return 0
-
-print("batata")
