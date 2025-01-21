e995cc7f7ef8f057c3859d7ecbaef8ace7bb07b9
Vinicius Coscia <vinicius.coscia@live.com>
2025-01-21 20:12:24 -0300

    Remove Duplicates

--- leetcode/80RemoveDuplicatesII.py
+++ leetcode/80RemoveDuplicatesII.py
@@ -1,3 +1,21 @@
+def minRemoveToMakeValid1(s: str) -> str:
+    if len(s) < 2:
+        return ""
+
+    stack = []
+    i = 0
+
+    while i < len(s):
+        if s[i] == '(':
+            stack.append(i)
+        elif s[i] == ')':
+            if not stack:
+                s = s[:i] + s[i + 1:]
+            else:
+                stack.pop()
+        i += 1
+    return s
+
 def minRemoveToMakeValid(s: str) -> str:
     indexes_to_remove = set()
     stack = []
