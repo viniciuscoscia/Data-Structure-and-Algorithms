3838e7b9d7ac95a91655f1d81a923980c4e704b1
Vinicius Coscia <vinicius.coscia@live.com>
2025-01-28 19:19:50 -0300

    Working LRU Cache. FINALLY!!!

--- leetcode/146LRUCache.py
+++ leetcode/146LRUCache.py
@@ -27,19 +27,18 @@ class LRUCache:
     def put(self, key: int, value: int) -> None:
         print("Put: " + str(key))
 
-        node = Node(key, value)
         if key in self.lruDict:
             node = self.lruDict[key]
             node.value = value
             self.linkedList.move_node_to_head(node)
         else:
+            node = Node(key, value)
             removed_node = self.linkedList.insert_front(node)
             if removed_node:
                 self.lruDict.pop(removed_node.key)
             self.lruDict[key] = node
 
         self.print_all()
-
 
     def print_all(self):
         print("Head = Key: ", end='')
@@ -51,13 +50,19 @@ class LRUCache:
         print(self.linkedList.tailNode.value)
 
         node = self.linkedList.headNode
+        print("Head to tail:")
         for i in range(self.linkedList.elements):
             print("[" + str(node.key) + ", " + str(node.value) + "] ", end='', sep=' ')
             node = node.next
 
+        node = self.linkedList.tailNode
+        print("\nTail to head:")
+        for i in range(self.linkedList.elements):
+            print("[" + str(node.key) + ", " + str(node.value) + "] ", end='', sep=' ')
+            node = node.previous
+
         print("")
         print("_________________")
-
 
 
 class Node:
@@ -67,7 +72,6 @@ class Node:
         self.next = None
         self.previous = None
 
-
 class DoubleLinkedList:
     def __init__(self, capacity: int):
         self.headNode: Optional[Node] = None
@@ -75,32 +79,41 @@ class DoubleLinkedList:
         self.capacity = capacity
         self.elements = 0
 
-    def move_node_to_head(self, node: Node):
-        if self.headNode == node or self.elements < 2:
+    def move_node_to_head(self, new_head: Node):
+        if self.headNode == new_head:
             return
 
+        if self.tailNode.key == new_head.key:
+            if self.elements > 2:
+                self.tailNode = self.tailNode.previous
+                new_head.next = self.headNode
+                self.headNode = new_head
+                return
+            else:
+                self.tailNode = self.headNode
+                self.headNode = new_head
+                return
+
         old_head = self.headNode
-        self.headNode = node
 
-        if self.headNode == self.tailNode:
-            self.tailNode = self.tailNode.previous
-            self.tailNode.next = self.headNode
+        if old_head.next.key == new_head.key:
+            old_head.next = new_head.next
+            new_head.next.previous = old_head
 
-        if self.tailNode.previous == self.headNode:
-            self.tailNode.previous = self.headNode.previous
+        if self.tailNode.previous.key == new_head.key:
+            self.tailNode.previous = new_head.previous
 
-        old_head.previous = self.headNode
-        if old_head.next == self.headNode:
-            old_head.next = self.headNode.next
+        previous = new_head.previous
+        new_head.next.previous = previous
+        previous.next = new_head.next
 
-        self.headNode.previous = self.tailNode
-        self.headNode.next = old_head
+        new_head.next = old_head
+        new_head.previous = self.tailNode
 
-        self.tailNode.next = self.headNode
+        old_head.previous = new_head
+        self.tailNode.next = new_head
+        self.headNode = new_head
 
-    '''
-    Returns Last Node Tail if limit is reached
-    '''
 
     def insert_front(self, node: Node) -> [Node]:
         if not self.headNode:
@@ -115,6 +128,7 @@ class DoubleLinkedList:
         old_head.previous = node
 
         self.headNode = node
+        self.headNode.previous = self.tailNode
         node.next = old_head
 
         if self.elements < self.capacity:
@@ -129,10 +143,21 @@ class DoubleLinkedList:
             self.headNode.previous = self.tailNode
             return removed_tail
 
-    def is_full_capacity(self) -> bool:
-        return self.capacity == self.elements
 
 
+print("\n\n--------------------------- TEST 1 ---------------------------\n\n")
+lru_cache = LRUCache(2)
+lru_cache.put(1,1)
+lru_cache.put(2,2)
+lru_cache.get(1)
+lru_cache.put(3,3)
+lru_cache.get(2)
+lru_cache.put(4,4)
+lru_cache.get(1)
+lru_cache.get(3)
+lru_cache.get(4)
+
+print("\n\n--------------------------- TEST 2 ---------------------------\n\n")
 lru_cache = LRUCache(3)
 lru_cache.put(1,1)
 lru_cache.put(2,2)
@@ -143,6 +168,26 @@ lru_cache.get(3)
 lru_cache.get(2)
 lru_cache.get(1)
 lru_cache.put(5,5)
+lru_cache.get(1)
+lru_cache.get(2)
+lru_cache.get(3)
+lru_cache.get(4)
+lru_cache.get(5)
+
+print("\n\n--------------------------- TEST 3 ---------------------------\n\n")
+lru_cache = LRUCache(5)
+lru_cache.put(1,1)
+lru_cache.put(2,2)
+lru_cache.put(3,3)
+lru_cache.put(4,4)
+lru_cache.put(5,5)
+lru_cache.put(6,6)
+lru_cache.get(1)
+lru_cache.get(4)
+lru_cache.get(3)
+lru_cache.get(2)
+lru_cache.put(6,66)
+lru_cache.put(4,44)
 lru_cache.get(1)
 lru_cache.get(2)
 lru_cache.get(3)
