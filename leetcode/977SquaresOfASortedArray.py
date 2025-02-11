e71134f2ed40c65c2d01a92b88d27ddf6d2ea43e
Vinicius Coscia <vinicius.coscia@live.com>
2025-02-10 23:48:07 -0300

    977 Squares of A Sorted Array

---
+++ leetcode/977SquaresOfASortedArray.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        right_pointer = len(nums) - 1
+        left_pointer = 0
+        index = 0
+        result = []
+
+        while right_pointer > left_pointer:
+            left_number = abs(nums[left_pointer])
+            right_number = abs(nums[right_pointer])
+
+            if left_number > right_number:
+                result[index] = left_number * left_number
+                left_pointer += 1
+            else:
+                result[index] = right_number * right_number
+                right_pointer += 1
+
+            index += 1
+
+        return result
